9f2edd70819e6fe2ed9a3ca69a15f42eeb7d0611
David Turissini <dturissini@gmail.com>
2024-07-17 15:48:58 -0400

    reworked outlier site database table and related code to have a separate pop field rather than hardcoding columns for allele counts for each pop

--- d_stats/windows/get_outlier_win_sites.py
+++ d_stats/windows/get_outlier_win_sites.py
@@ -17,29 +17,15 @@ def create_windows_table(conn, win_size, outlier_type, sites_table):
                    win_id varchar(30),
                    chrom varchar(20),
                    pos int,
-                   mel_allele varchar(1),
-                   total_alleles_sim int,
-                   total_alleles_ssh int,
-                   total_alleles_sech int,
-                   total_alleles_sech_anro int,
-                   total_alleles_sech_denis int,
-                   total_alleles_sech_ladigue int,
-                   total_alleles_sech_marianne int,
-                   total_alleles_sech_praslin int,
-                   total_alleles_sech_unknown int,
-                   der_alleles_sim int,
-                   der_alleles_ssh int,
-                   der_alleles_sech int,
-                   der_alleles_sech_anro int,
-                   der_alleles_sech_denis int,
-                   der_alleles_sech_ladigue int,
-                   der_alleles_sech_marianne int,
-                   der_alleles_sech_praslin int,
-                   der_alleles_sech_unknown int)""")
+                   outgroup_allele varchar(1),
+                   pop varchar(50),
+                   total_alleles int,
+                   der_alleles int)""")
 
                    
   cur.execute(f"create index idx_odwps_pos_{win_size}{outlier_type} on {sites_table}(pos)")
-  cur.execute(f"create index idx_odwps_dsw_{win_size}{outlier_type} on {sites_table}(win_id)")
+  cur.execute(f"create index idx_odwps_win_id_{win_size}{outlier_type} on {sites_table}(win_id)")
+  cur.execute(f"create index idx_odwps_pop_{win_size}{outlier_type} on {sites_table}(pop)")
 
   cur.close()
   
@@ -135,133 +121,76 @@ def main():
   
   #get outgroup
   outgroup = pop_str.split('_')[3]
-
-
+  
+  
   # Intialize pop dictionary.
   idx_pop_dicc = {}
   for pop in list(set(meta_df['pop'])):
     idx_pop_dicc[pop] = meta_df['vcf_order'][meta_df['pop'] == pop]
       
+  focal_pops = list(set(meta_df['pop']))
+  focal_pops.remove(outgroup)
   
-  #define empty lists corresponding to database fields
-  odwps_ids = []
-  win_ids = []
-  chroms = []
-  positions = []
-  outgroup_alleles = []
-  total_sims = []
-  total_sshs = []
-  total_sechs = []
-  total_sechs_anro = []
-  total_sechs_denis = []
-  total_sechs_ladigue = []
-  total_sechs_marianne = []
-  total_sechs_praslin = []
-  total_sechs_unknown = []
-  der_sims = []
-  der_sshs = []
-  der_sechs = []
-  der_sechs_anro = []
-  der_sechs_denis = []
-  der_sechs_ladigue = []
-  der_sechs_marianne = []
-  der_sechs_praslin = []
-  der_sechs_unknown = []
- 
- 
   
+
   # calculate stats for every outlier window
   odwps_id = 0
   for idx in range(win_df.shape[0]):
-      win_id = win_df.win_id.values[idx]
-      chrom = win_df.chrom.values[idx]
-      start = win_df.start.values[idx]
-      end = win_df.end.values[idx]
-       
-      # Extract the genotype callset and positions.
-      zarr_file = zarr_prefix + '_' + chrom + '.zarr'
-      callset, all_pos = load_callset_pos(chrom, zarr_file)
-      # Identify the window to extract.
-      wind_loc = all_pos.locate_range(start, end)
-      win_gt = allel.GenotypeArray(callset[wind_loc])
-      
+    win_id = win_df.win_id.values[idx]
+    chrom = win_df.chrom.values[idx]
+    start = win_df.start.values[idx]
+    end = win_df.end.values[idx]
+     
+    # Extract the genotype callset and positions.
+    zarr_file = zarr_prefix + '_' + chrom + '.zarr'
+    callset, all_pos = load_callset_pos(chrom, zarr_file)
+    # Identify the window to extract.
+    wind_loc = all_pos.locate_range(start, end)
+    win_gt = allel.GenotypeArray(callset[wind_loc])
+    
+    for pop in focal_pops:
+      #define empty lists corresponding to database fields
+      odwps_ids = []
+      win_ids = []
+      chroms = []
+      positions = []
+      outgroup_alleles = []
+      pops = []
+      total_alleles = []
+      der_alleles = []
 
+    
       # Compute derived allele freq
-      total_sim, der_sim = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sim'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_ssh, der_ssh = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['ssh'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_sech, der_sech = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sech'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-
-      total_sech_anro, der_sech_anro = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sechanro'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_sech_denis, der_sech_denis = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sechden'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_sech_ladigue, der_sech_ladigue = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sechlad'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_sech_marianne, der_sech_marianne = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sechmari'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_sech_praslin, der_sech_praslin = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sechpras'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-      total_sech_unknown, der_sech_unknown = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc['sechunk'], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
-
- 
+      total_alleles, der_alleles = get_der_allele_counts(gt=win_gt.take(idx_pop_dicc[pop], axis=1), outgroup_gt=win_gt.take(idx_pop_dicc[outgroup], axis=1))
+      
+      #get outgroup alleles
+      outgroup_alleles.extend(win_gt.take(idx_pop_dicc[outgroup], axis=1)[:, 0, 0])
+      
       for pos in all_pos[wind_loc]:
         odwps_id += 1
         odwps_ids.append(odwps_id)
         win_ids.append(win_id)
         chroms.append(chrom)
         positions.append(pos)
+        pops.append(pop)
         
-
-      outgroup_alleles.extend(win_gt.take(idx_pop_dicc[outgroup], axis=1)[:, 0, 0])
+      #create dataframe using lists that can be loaded into the database table           
+      site_df = pd.DataFrame()    
+      #add columns to site_df
       
-      #add values to lists
-      total_sims.extend(total_sim)
-      total_sshs.extend(total_ssh)
-      total_sechs.extend(total_sech)
-      total_sechs_anro.extend(total_sech_anro)
-      total_sechs_denis.extend(total_sech_denis)
-      total_sechs_ladigue.extend(total_sech_ladigue)
-      total_sechs_marianne.extend(total_sech_marianne)
-      total_sechs_praslin.extend(total_sech_praslin)
-      total_sechs_unknown.extend(total_sech_unknown)
-      
-      der_sims.extend(der_sim)
-      der_sshs.extend(der_ssh)
-      der_sechs.extend(der_sech)
-      der_sechs_anro.extend(der_sech_anro)
-      der_sechs_denis.extend(der_sech_denis)
-      der_sechs_ladigue.extend(der_sech_ladigue)
-      der_sechs_marianne.extend(der_sech_marianne)
-      der_sechs_praslin.extend(der_sech_praslin)
-      der_sechs_unknown.extend(der_sech_unknown)
-
-  #create dataframe using lists that can be loaded into the database table           
-  site_df = pd.DataFrame()    
-  #add columns to site_df
-  site_df["odwps_id"] = odwps_ids  
-  site_df["win_id"] = win_ids  
-  site_df["chrom"] = chroms  
-  site_df["pos"] = positions
-  site_df["mel_allele"] = outgroup_alleles
-  site_df["total_alleles_sim"] = total_sims
-  site_df["total_alleles_ssh"] = total_sshs
-  site_df["total_alleles_sech"] = total_sechs
-  site_df["total_alleles_sech_anro"] = total_sechs_anro
-  site_df["total_alleles_sech_denis"] = total_sechs_denis
-  site_df["total_alleles_sech_ladigue"] = total_sechs_ladigue
-  site_df["total_alleles_sech_marianne"] = total_sechs_marianne
-  site_df["total_alleles_sech_praslin"] = total_sechs_praslin
-  site_df["total_alleles_sech_unknown"] = total_sechs_unknown
-  site_df["der_alleles_sim"] = der_sims
-  site_df["der_alleles_ssh"] = der_sshs
-  site_df["der_alleles_sech"] = der_sechs
-  site_df["der_alleles_sech_anro"] = der_sechs_anro
-  site_df["der_alleles_sech_denis"] = der_sechs_denis
-  site_df["der_alleles_sech_ladigue"] = der_sechs_ladigue
-  site_df["der_alleles_sech_marianne"] = der_sechs_marianne
-  site_df["der_alleles_sech_praslin"] = der_sechs_praslin
-  site_df["der_alleles_sech_unknown"] = der_sechs_unknown
-
-  
-  
-  #import site_df to db
-  conn = sqlite3.connect(db_file)
-  site_df.to_sql(sites_table, if_exists = 'append', index=False, con=conn)
+      site_df["odwps_id"] = odwps_ids  
+      site_df["win_id"] = win_ids  
+      site_df["chrom"] = chroms  
+      site_df["pos"] = positions
+      site_df["outgroup_allele"] = outgroup_alleles
+      site_df["pop"] = pops
+      site_df["total_alleles"] = total_alleles
+      site_df["der_alleles"] = der_alleles
+           
+      #import site_df to db
+      conn = sqlite3.connect(db_file)
+      site_df.to_sql(sites_table, if_exists = 'append', index=False, con=conn)
+    
   conn.commit()
   conn.close()
   
--- d_stats/windows/ssh_outlier_wins.R
+++ d_stats/windows/ssh_outlier_wins.R
@@ -8,7 +8,7 @@ win_size <- as.character(args[1])
 outlier_type <- args[2]
 pop_str <- args[3]
 
-\
+
 #define file paths
 base_dir <- '/proj/matutelb/projects/drosophila/sim_sech_hybrid/introgression/d_stats/windows/'
 db_file <- paste(base_dir, 'ssh_d_win.db', sep='')
@@ -36,13 +36,15 @@ d_wins <- dbGetQuery(conn, paste("select *
                                   from ", d_stats_table, "
                                   where d_plus is not null", sep=''))
 
-win_sites <- dbGetQuery(conn, paste("select *, 
-                                     der_alleles_sim / total_alleles_sim der_freq_sim,
-                                     der_alleles_ssh / total_alleles_ssh der_freq_ssh,
-                                     der_alleles_sech / total_alleles_sech der_freq_sech 
+win_sites <- dbGetQuery(conn, paste("select *
                                      from ", win_site_table, "
+                                     where der_alleles > 0
                                      order by chrom, pos", sep=''))
 
+#get pops and colors for plotting
+pop_cols <- dbGetQuery(conn, paste("select pop, short_desc, col
+                                     from pop_cols
+                                     order by pop", sep=''))
 
 gwas_pos <- dbGetQuery(conn, paste("select win_id, g.chrom, pos, p, start, end, d_plus
                                     from gwas_snp g, ", d_stats_table, " w
@@ -119,126 +121,70 @@ genes <- dbGetQuery(conn, paste("select w.win_id, gi.start, gi.end, group_concat
 
 dbSendQuery(conn, paste("drop table if exists", tmp_dsw_table))
 
+#define pop vectors
+species_pops <- c('sim', 'sech', 'ssh')
+sech_pops <- c('sechanro', 'sechden', 'sechlad', 'sechmari', 'sechpras', 'sechunk')
+
+
+
 #plot derived allele freqs for each outlier window
 pdf(pdf_file, height=8, width=10.5)
-layout(matrix(c(1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 6, 7, 7, 7, 8, 9, 9, 9, 10), nrow=5, byrow=T))
+layout(matrix(c(1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 6, 7, 7, 7, 8), nrow=4, byrow=T))
 for (win_id in sort(unique(win_sites$win_id)))
   {
-  #sim derived allele freqs
+  # derived allele freqs
   par(mar=c(0, 4.1, 4.1, 0))
-  plot(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sim[win_sites$win_id == win_id] / win_sites$total_alleles_sim[win_sites$win_id == win_id], pch=20, col='blue', xaxt='n', xlab='', ylim=c(0,1), ylab='Derived freq', main='')
-  abline(v=gwas_pos$pos[gwas_pos$win_id == win_id], col='black')
-  if (sum(genes$win_id == win_id) > 0)
-    {
-    rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)
-    mtext(substr(genes$synonyms[genes$win_id == win_id], 1, 30), side = 3, line = 0:2, at = (sapply(genes$start[genes$win_id == win_id], max, min(win_sites$pos[win_sites$win_id == win_id])) + sapply(genes$end[genes$win_id == win_id], min, max(win_sites$pos[win_sites$win_id == win_id]))) / 2, cex=.5)
-    }
-
-  par(mar=c(0, 0, 4.1, .1))
-  sim_hist <- hist(win_sites$der_alleles_sim[win_sites$win_id == win_id & win_sites$der_alleles_sim > 0] / win_sites$total_alleles_sim[win_sites$win_id == win_id & win_sites$der_alleles_sim > 0], breaks=seq(0, 1.01, .01), plot=F)
-  plot(1, type='n', xlim=c(0, max(sim_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', main='')
-  polygon(c(0, 0, sim_hist$counts, 1), c(0, sim_hist$mids[1], sim_hist$mids, sim_hist$mids[length(sim_hist$mids)]), col='blue', border=NA)  
-  legend('topright', 'sim', fill='blue', border=NA, cex=3, bty='n')
-  mtext(paste('D+ = ', round(d_wins$d_plus[d_wins$win_id == win_id], 2), ' (', round(sum(d_wins$d_plus[d_wins$win_id == win_id] > d_wins$d_plus) / length(d_wins$d_plus), 3), ' percentile)', sep=''), 0, side=3, line=1, at=0, adj=0)
+  counter <- 0
+  for (pop in species_pops)
+    {  
+    counter <- counter + 1
     
-  #ssh derived allele freqs
-  par(mar=c(0, 4.1, 0, 0))
-  plot(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_ssh[win_sites$win_id == win_id] / win_sites$total_alleles_ssh[win_sites$win_id == win_id], pch=20, col='gold', xaxt='n', xlab='', ylim=c(0,1), ylab='Derived freq', main='')
-
-  abline(v=gwas_pos$pos[gwas_pos$win_id == win_id], col='black')
-  if (sum(genes$win_id == win_id) > 0)
-    {
-    rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)
+    plot(win_sites$pos[win_sites$win_id == win_id & win_sites$pop == pop], win_sites$der_alleles[win_sites$win_id == win_id & win_sites$pop == pop] / win_sites$total_alleles[win_sites$win_id == win_id & win_sites$pop == pop], pch=20, col=pop_cols$col[pop_cols$pop == pop], xaxt='n', xlab='', ylim=c(0,1), ylab='Derived freq', main='')
+    abline(v=gwas_pos$pos[gwas_pos$win_id == win_id], col='black')
+    if (sum(genes$win_id == win_id) > 0)
+      {
+      rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)
+      if (counter == 1)
+        {mtext(substr(genes$synonyms[genes$win_id == win_id], 1, 30), side = 3, line = 0:2, at = (sapply(genes$start[genes$win_id == win_id], max, min(win_sites$pos[win_sites$win_id == win_id])) + sapply(genes$end[genes$win_id == win_id], min, max(win_sites$pos[win_sites$win_id == win_id]))) / 2, cex=.5)}
+      }
+    
+    if (counter == 1)
+      {
+      par(mar=c(0, 0, 4.1, .1))
+      } else {
+      par(mar=c(0, 0, 0, .1))
+      }
+      
+    pop_hist <- hist(win_sites$der_alleles[win_sites$win_id == win_id & win_sites$pop == pop] / win_sites$total_alleles[win_sites$win_id == win_id & win_sites$pop == pop], breaks=seq(0, 1.01, .01), plot=F)
+    plot(1, type='n', xlim=c(0, max(pop_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', main='')
+    polygon(c(0, 0, pop_hist$counts, 1), c(0, pop_hist$mids[1], pop_hist$mids, pop_hist$mids[length(pop_hist$mids)]), col=pop_cols$col[pop_cols$pop == pop], border=NA)  
+    legend('topright', pop, fill=pop_cols$col[pop_cols$pop == pop], border=NA, cex=3, bty='n')
+    if (counter == 1)
+      {mtext(paste('D+ = ', round(d_wins$d_plus[d_wins$win_id == win_id], 2), ' (', round(sum(d_wins$d_plus[d_wins$win_id == win_id] > d_wins$d_plus) / length(d_wins$d_plus), 3), ' percentile)', sep=''), 0, side=3, line=1, at=0, adj=0)}
+      
+    par(mar=c(0, 4.1, 0, 0))
     }
 
-  par(mar=c(0, 0, 0, .1))
-  ssh_hist <- hist(win_sites$der_alleles_ssh[win_sites$win_id == win_id & win_sites$der_alleles_ssh > 0] / win_sites$total_alleles_ssh[win_sites$win_id == win_id & win_sites$der_alleles_ssh > 0], breaks=seq(0, 1.01, .01), plot=F)
-  plot(1, type='n', xlim=c(0, max(ssh_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', main='')
-  polygon(c(0, 0, ssh_hist$counts, 1), c(0, ssh_hist$mids[1], ssh_hist$mids, ssh_hist$mids[length(ssh_hist$mids)]), col='gold', border=NA)  
-  legend('topright', 'ssh', fill='gold', border=NA, cex=3, bty='n')
-
-
-  #sech derived allele freqs
-  par(mar=c(0, 4.1, 0, 0))
-  plot(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech[win_sites$win_id == win_id] / win_sites$total_alleles_sech[win_sites$win_id == win_id], pch=20, col='red', xaxt='n', ylim=c(0,1), ylab='Derived freq', main='')
-  abline(v=gwas_pos$pos[gwas_pos$win_id == win_id], col='black')
-  if (sum(genes$win_id == win_id) > 0)
-    {
-    rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)
-    }
-
-  par(mar=c(0, 0, 0, .1))
-  sech_hist <- hist(win_sites$der_alleles_sech[win_sites$win_id == win_id & win_sites$der_alleles_sech > 0] / win_sites$total_alleles_sech[win_sites$win_id == win_id & win_sites$der_alleles_sech > 0], breaks=seq(0, 1.01, .01), plot=F)
-  plot(1, type='n', xlim=c(0, max(sech_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', main='')
-  polygon(c(0, 0, sech_hist$counts, 1), c(0, sech_hist$mids[1], sech_hist$mids, sech_hist$mids[length(sech_hist$mids)]), col='red', border=NA)  
-  legend('topright', 'sech', fill='red', border=NA, cex=3, bty='n')
-
-
-  #sech locations
-  par(mar=c(0, 4.1, 0, 0))
-  plot(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech_unknown[win_sites$win_id == win_id] / win_sites$total_alleles_sech_unknown[win_sites$win_id == win_id], pch=20, col='gray', xaxt='n', ylim=c(0,1), ylab='Derived freq', main='')
-
-  points(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech_anro[win_sites$win_id == win_id] / win_sites$total_alleles_sech_anro[win_sites$win_id == win_id], pch=20, col='orange')
-  points(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech_denis[win_sites$win_id == win_id] / win_sites$total_alleles_sech_denis[win_sites$win_id == win_id], pch=20, col='purple')
-  points(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech_ladigue[win_sites$win_id == win_id] / win_sites$total_alleles_sech_ladigue[win_sites$win_id == win_id], pch=20, col='green')
-  points(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech_marianne[win_sites$win_id == win_id] / win_sites$total_alleles_sech_marianne[win_sites$win_id == win_id], pch=20, col='darkblue')
-  points(win_sites$pos[win_sites$win_id == win_id], win_sites$der_alleles_sech_praslin[win_sites$win_id == win_id] / win_sites$total_alleles_sech_praslin[win_sites$win_id == win_id], pch=20, col='lightblue')
-
-
-  abline(v=gwas_pos$pos[gwas_pos$win_id == win_id], col='black')
-  if (sum(genes$win_id == win_id) > 0)
-    {
-    rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)
-    }
-
-  par(mar=c(0, 0, 0, .1))
-  sech_unknown_hist <- hist(win_sites$der_alleles_sech_unknown[win_sites$win_id == win_id & win_sites$der_alleles_sech_unknown > 0] / win_sites$total_alleles_sech_unknown[win_sites$win_id == win_id & win_sites$der_alleles_sech_unknown > 0], breaks=seq(0, 1.01, .01), plot=F)
-  sech_anro_hist <- hist(win_sites$der_alleles_sech_anro[win_sites$win_id == win_id & win_sites$der_alleles_sech_anro > 0] / win_sites$total_alleles_sech_anro[win_sites$win_id == win_id & win_sites$der_alleles_sech_anro > 0], breaks=seq(0, 1.01, .01), plot=F)
-  sech_denis_hist <- hist(win_sites$der_alleles_sech_denis[win_sites$win_id == win_id & win_sites$der_alleles_sech_denis > 0] / win_sites$total_alleles_sech_denis[win_sites$win_id == win_id & win_sites$der_alleles_sech_denis > 0], breaks=seq(0, 1.01, .01), plot=F)
-  sech_ladigue_hist <- hist(win_sites$der_alleles_sech_ladigue[win_sites$win_id == win_id & win_sites$der_alleles_sech_ladigue > 0] / win_sites$total_alleles_sech_ladigue[win_sites$win_id == win_id & win_sites$der_alleles_sech_ladigue > 0], breaks=seq(0, 1.01, .01), plot=F)
-  sech_marianne_hist <- hist(win_sites$der_alleles_sech_marianne[win_sites$win_id == win_id & win_sites$der_alleles_sech_marianne > 0] / win_sites$total_alleles_sech_marianne[win_sites$win_id == win_id & win_sites$der_alleles_sech_marianne > 0], breaks=seq(0, 1.01, .01), plot=F)
-  sech_praslin_hist <- hist(win_sites$der_alleles_sech_praslin[win_sites$win_id == win_id & win_sites$der_alleles_sech_praslin > 0] / win_sites$total_alleles_sech_praslin[win_sites$win_id == win_id & win_sites$der_alleles_sech_praslin > 0], breaks=seq(0, 1.01, .01), plot=F)
-  plot(1, type='n', xlim=c(0, max(sech_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', xlab='num sites', main='')
-  points(sech_unknown_hist$counts, sech_unknown_hist$mids, col='gray', type='l')  
-  points(sech_anro_hist$counts, sech_anro_hist$mids, col='orange', type='l')  
-  points(sech_denis_hist$counts, sech_denis_hist$mids, col='purple', type='l')  
-  points(sech_ladigue_hist$counts, sech_ladigue_hist$mids, col='green', type='l')  
-  points(sech_marianne_hist$counts, sech_marianne_hist$mids, col='darkblue', type='l')  
-  points(sech_praslin_hist$counts, sech_praslin_hist$mids, col='lightblue', type='l')  
-  legend('topright', c('sech - Unk', 'sech - Anro', 'sech - Denis', 'sech - La Digue', 'sech - Marianne', 'sech - Praslin'), fill=c('gray', 'orange', 'purple', 'green', 'darkblue', 'lightblue'), border=NA, cex=1, bty='n')
-
-  
-
-
-
-  #ssh freq diffs
+  #sech pops
   par(mar=c(5.1, 4.1, 0, 0))
-  ssh_sim_filter <- win_sites$win_id == win_id
-  ssh_sech_filter <- win_sites$win_id == win_id
-  
-  ssh_sim_diff <- abs(win_sites$der_alleles_ssh[ssh_sim_filter] / win_sites$total_alleles_ssh[ssh_sim_filter] - win_sites$der_alleles_sim[ssh_sim_filter] / win_sites$total_alleles_sim[ssh_sim_filter])
-  ssh_sech_diff <- abs(win_sites$der_alleles_ssh[ssh_sech_filter] / win_sites$total_alleles_ssh[ssh_sech_filter] - win_sites$der_alleles_sech[ssh_sech_filter] / win_sites$total_alleles_sech[ssh_sech_filter])
-  
-  plot(win_sites$pos[ssh_sim_filter], ssh_sim_diff, pch=20, col='green', xlab=paste('Pos on', d_wins$chrom[d_wins$win_id == win_id]), ylim=c(0,1), ylab='Derived freq diff', main='')
-  points(win_sites$pos[ssh_sech_filter], ssh_sech_diff, pch=20, col='orange')
-
-
+  plot(1, type='n', xlim=range(win_sites$pos[win_sites$win_id == win_id]), ylim=c(0,1), ylab='Derived freq', xlab=paste('Position on ', win_sites$chrom[win_sites$win_id == win_id], sep=''), main='')
   abline(v=gwas_pos$pos[gwas_pos$win_id == win_id], col='black')
   if (sum(genes$win_id == win_id) > 0)
-    {
-    rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)
-    }
+    {rect(genes$start[genes$win_id == win_id], -1, genes$end[genes$win_id == win_id], 2, col=adjustcolor('gray', .2), border=NA)}
 
+  for (pop in sech_pops)
+    {    
+    points(win_sites$pos[win_sites$win_id == win_id & win_sites$pop == pop], win_sites$der_alleles[win_sites$win_id == win_id & win_sites$pop == pop] / win_sites$total_alleles[win_sites$win_id == win_id & win_sites$pop == pop], pch=20, col=pop_cols$col[pop_cols$pop == pop])        
+    }
+    
   par(mar=c(5.1, 0, 0, .1))
-  ssh_sim_hist <- hist(ssh_sim_diff, breaks=seq(0, 1.01, .01), plot=F)
-  ssh_sech_hist <- hist(ssh_sech_diff, breaks=seq(0, 1.01, .01), plot=F)
-  plot(1, type='n', xlim=c(0, max(sech_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', xlab='num sites', main='')
-  points(ssh_sim_hist$counts, ssh_sim_hist$mids, col='green', type='l')  
-  points(ssh_sech_hist$counts, ssh_sech_hist$mids, col='orange', type='l')  
-  legend('topright', c('ssh - sim', 'ssh - sech'), fill=c('green', 'orange'), border=NA, cex=1, bty='n')
-  abline(h=mean(ssh_sim_diff), col=adjustcolor('green', .4))
-  abline(h=mean(ssh_sech_diff), col=adjustcolor('orange', .4))
-  
+  plot(1, type='n', xlim=c(0, max(pop_hist$counts)), ylim=c(0, 1), xaxt='n', yaxt='n', xlab='num sites', main='')
+  for (pop in sech_pops)
+    {    
+    pop_hist <- hist(win_sites$der_alleles[win_sites$win_id == win_id & win_sites$pop == pop] / win_sites$total_alleles[win_sites$win_id == win_id & win_sites$pop == pop], breaks=seq(0, 1.01, .01), plot=F)
+    points(pop_hist$counts, pop_hist$mids, col=pop_cols$col[pop_cols$pop == pop], type='l')  
+    }
+  legend('topright', pop_cols$short_desc[pop_cols$pop %in% sech_pops], fill=pop_cols$col[pop_cols$pop %in% sech_pops], border=NA, cex=1, bty='n')
   par(mar=c(5.1, 4.1, 4.1, 2.1))
   }  
 layout(1)
