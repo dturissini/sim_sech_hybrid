4a054453c2b206d7b63a83ba2f1ec1232c27371f
David Turissini <dturissini@gmail.com>
2024-10-02 09:25:44 -0700

    zarr file now contains ref and alt alleles

--- d_stats/windows/filtered_vcf_to_zarr.py
+++ d_stats/windows/filtered_vcf_to_zarr.py
@@ -15,5 +15,5 @@ zarr_path = str(sys.argv[3])
 # Convert the vcf file to a zarr array.
 allel.vcf_to_zarr(
     vcf_path, zarr_path, group=str(chrom),
-    fields=['samples', 'DP', 'POS', 'CHROM', 'GT'], log=sys.stdout, overwrite=True,
+    fields=['samples', 'DP', 'POS', 'CHROM', 'GT', 'variants/REF', 'variants/ALT'], log=sys.stdout, overwrite=True,
 )
