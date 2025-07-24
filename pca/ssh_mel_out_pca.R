ce92e6734a781fa42e31458d2d4fb3bd195c4efe
David Turissini <dturissini@gmail.com>
2025-07-24 10:32:03 -0700

    updated queries to get samples pops and colors

--- pca/ssh_mel_out_pca.R
+++ pca/ssh_mel_out_pca.R
@@ -28,37 +28,43 @@ db_file <- '/work/users/d/t/dturissi/drosophila/ssh/introgression/d_stats/window
 conn <- dbConnect(dbDriver("SQLite"), db_file)
 
 #all
-all_pops <- dbGetQuery(conn, "select sample_id, l.pop, col
-                              from lk_pop l, pop_cols c
-                              where l.pop in ('sim', 'sech', 'ssh', 'mel')
+all_pops <- dbGetQuery(conn, "select s.sample_id, l.pop, col
+                              from sample_species s, sample_pop_link l, pop_cols c
+                              where s.sample_id = l.sample_id
+                              and l.pop in ('sim', 'sech', 'ssh', 'mel')
                               and l.pop = c.pop")
                               
 pca_plots('sim_sech_outgroup_mel_pca', all_pops)
+pca_plots('sim_sech_outgroup_mel_all_sites_pca', all_pops)
 
 
 
 #sech pops
-sech_pops <- dbGetQuery(conn, "select sample_id, l.pop, col
-                              from lk_pop l, pop_cols c
-                              where l.pop in ('sechden', 'sechpras', 'sechanro', 'sechlad', 'sechunk', 'sechmari')
+sech_pops <- dbGetQuery(conn, "select s.sample_id, l.pop, col
+                              from sample_species s, sample_pop_link l, pop_cols c
+                              where s.sample_id = l.sample_id
+                              and l.pop in ('sechden', 'sechpras', 'sechanro', 'sechlad', 'sechunk', 'sechmari')
                               and l.pop = c.pop")
 
 pca_plots('sech_pca', sech_pops)
+pca_plots('sech_all_sites_pca', sech_pops)
 
 
 #sechbase pops
-sechbase_pops <- dbGetQuery(conn, "select sample_id, l.pop, col
-                                   from lk_pop l, pop_cols c
-                                   where l.pop in ('sechanro', 'sechlad', 'sechmari')
+sechbase_pops <- dbGetQuery(conn, "select s.sample_id, l.pop, col
+                                   from sample_species s, sample_pop_link l, pop_cols c
+                                   where s.sample_id = l.sample_id
+                                   and l.pop in ('sechanro', 'sechlad', 'sechmari')
                                    and l.pop = c.pop")
 
 pca_plots('sechbase_pca', sechbase_pops)
 
 
 #sech_ssh
-sech_ssh_pops <- dbGetQuery(conn, "select sample_id, l.pop, col
-                                   from lk_pop l, pop_cols c
-                                   where l.pop in ('sshlad', 'sshmahe', 'sshanro', 'sechden', 'sechpras', 'sechanro', 'sechlad', 'sechunk', 'sechmari')
+sech_ssh_pops <- dbGetQuery(conn, "select s.sample_id, l.pop, col
+                                   from sample_species s, sample_pop_link l, pop_cols c
+                                   where s.sample_id = l.sample_id
+                                   and l.pop in ('sshlad', 'sshmahe', 'sshanro', 'sechden', 'sechpras', 'sechanro', 'sechlad', 'sechunk', 'sechmari')
                                    and l.pop = c.pop")
 
 pca_plots('sech_ssh_pca', sech_ssh_pops)
